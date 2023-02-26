0e01587d8246b2cdc9778f3f9afe6d5e91f626bf
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-02-26 16:17:42 -0600

    creating engineer test file

---
+++ Tests/Engineer.test.js
@@ -0,0 +1,23 @@
+class Engineer {
+    constructor(name, id, email, role, github) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.github = github;
+    }
+};
+
+
+describe("Engineer", () => {
+    it("returns an object that includes the engineer properties.", () => {
+        const engineer = new Engineer("John Wick", 1, "johnwick@email.com", "johnwick1", "Engineer")
+        expect(engineer.name).toEqual("John Wick")
+        expect(engineer.id).toEqual(1)
+        expect(engineer.email).toEqual("johndoe@email.com")
+        expect(engineer.github).toEqual("johndoe1")
+        expect(engineer.role).toEqual("Engineer")
+        expect(engineer.officeNumber).toEqual(undefined)
+        expect(engineer.school).toEqual(undefined)
+    })
+});
