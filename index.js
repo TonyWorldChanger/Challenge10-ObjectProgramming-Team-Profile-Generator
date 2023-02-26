faf76a46f484371d58c544f5e3341c9d10e3109a
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-02-26 17:17:54 -0600

    creating file for html

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateProfile = require("./generateProfile");
-
+const generateProfile = require("./dist/generateProfile");
 
 
 
