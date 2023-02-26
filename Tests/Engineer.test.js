ce2358a7556143399d7c78b0fd0508eb92e4c364
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-02-26 16:24:10 -0600

    creating intern test file

--- Tests/Engineer.test.js
+++ Tests/Engineer.test.js
@@ -12,12 +12,10 @@ class Engineer {
 describe("Engineer", () => {
     it("returns an object that includes the engineer properties.", () => {
         const engineer = new Engineer("John Wick", 1, "johnwick@email.com", "johnwick1", "Engineer")
-        expect(engineer.name).toEqual("John Wick")
-        expect(engineer.id).toEqual(1)
-        expect(engineer.email).toEqual("johndoe@email.com")
-        expect(engineer.github).toEqual("johndoe1")
-        expect(engineer.role).toEqual("Engineer")
-        expect(engineer.officeNumber).toEqual(undefined)
-        expect(engineer.school).toEqual(undefined)
-    })
+        expect(engineer.name).toEqual("John Wick");
+        expect(engineer.id).toEqual(1);
+        expect(engineer.email).toEqual("johndoe@email.com");
+        expect(engineer.github).toEqual("johndoe1");
+        expect(engineer.role).toEqual("Engineer");
+    });
 });
---
+++ Tests/Intern.test.js
@@ -0,0 +1,19 @@
+class Intern {
+    constructor(name, id, email, role, school) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.school = school;
+    }
+};
+
+ describe("Intern", () => {
+        it("returns an object with the properties of Employee, in addition to the Intern properties.", () => {
+            const intern = new Intern("John Wick", 1, "johnwick@email.com", "University of Wick")
+            expect(intern.name).toEqual("John Wick");
+            expect(intern.id).toEqual(1);
+            expect(intern.email).toEqual("johnwick@wmail.com");
+            expect(intern.school).toEqual("University of Wick");
+        });
+     });
