e4df3fd277c5bb589d7b8c9bb0c2d28390dd490f
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-02-26 16:33:34 -0600

    creating manager test file

---
+++ Tests/Manager.test.js
@@ -0,0 +1,20 @@
+class Manager {
+    constructor(name, id, email, role, officeNumber) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.officeNumber = officeNumber;
+    }
+};
+
+describe("Manager", () => {
+    it("returns an object with the properties of Employee, in addition to the Intern properties.", () => {
+        const manager = new Manager("John Wick", 1, "johnwick@email.com", "officeNumber", "Manager")
+        expect(manager.name).toEqual("John Wick")
+        expect(manager.id).toEqual(1)
+        expect(manager.email).toEqual("johnwick@email.com")
+        expect(manager.officeNumber).toEqual(undefined)
+        expect(manager.role).toEqual("Manger");
+    });
+ });
