ef2e2f86ec9e140558348365434654e8d546740b
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-03-11 21:53:49 -0600

    creating function to build team

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateProfile = require("./src/generateProfile");
-const { off } = require("process");
+const path = require("path");
+const DIST_DIR = path.resolve(_dirname, "dist");
+const distpath = path.join(DIST_DIR, index.html);
 const roundTable = [];
 
 
@@ -77,6 +78,19 @@ const promptManager = () => {
       },
       {
          type: "input",
+         name: "email",
+         message: "Enter your email address",
+         validate: email => {
+            if (email) {
+               return true;
+            } else {
+               console.log("Please enter email address");
+               return false;
+            }
+         }
+      },
+      {
+         type: "input",
          name: "officeNumber",
          message: "Enter your current office number",
          validate: officeNumber => {
@@ -90,7 +104,7 @@ const promptManager = () => {
       }
    ]).then(answers => {
       console.log(answers);
-      const manager = new manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
+      const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
       roundTable.push(manager);
       promptTeamMenu();
    })
@@ -116,36 +130,144 @@ const promptTeamMenu = () => {
             promptIntern();
             break;
          default:
-            assembleAvenger();
+            assembleAvengers();
 
       }
    });
 };
 
-
-
-
-function writeToFile(fileName, data) {
-    if (!fs.existsSync(DIST_DIR)) {
-        fs.mkdirSync(DIST_DIR);
-    }
-    fs.writeFileSync(disPath, render(teamMembers), "utf-8");
-
-    return fs.writeFileSync(index.html, fileName), data;
+const promptEngineer = () => {
+   inquirer.prompt([
+      {
+         type:"input",
+         name:"name",
+         message:"What is the employee's name?", 
+         validate: nameInput => {
+            if (nameInput) {
+               return true;
+            } else {
+               console.log("Please enter your full name!");
+               return false;
+            }
+         }
+      },
+      {
+         type: "input",
+         name: "employeeId",
+         message: "Enter your employee ID",
+         validate: employeeId => {
+            if (employeeId) {
+               return true;
+            } else {
+               console.log("Please enter employee ID");
+               return false;
+            }
+         }
+      },
+      {
+         type: "input",
+         name: "email",
+         message: "Enter your email address",
+         validate: email => {
+            if (email) {
+               return true;
+            } else {
+               console.log("Please enter email address");
+               return false;
+            }
+         }
+      },
+      {
+         type: "input",
+         name: "github",
+         message: "Enter your GitHub username",
+         validate: github => {
+            if (github) {
+               return true;
+            } else {
+               console.log("Please enter your GitHub username");
+               return false;
+            }
+         }
+      }
+   ]).then(answers => {
+      console.log(answers);
+      const engineer = new Engineer(answers.name, answers.employeeId, answers.email, answers.github);
+      roundTable.push(engineer);
+      promptTeamMenu();
+   })
 };
 
-
-function init () {
-    inquirer.prompt(questions).then((res) => {
-        console.log("Team Profile File...");
-        writeToFile("./dist/generateProfile.js");
-    });
-    
+const promptIntern = () => {
+   inquirer.prompt([
+      {
+         type:"input",
+         name:"name",
+         message:"What is the employee's name?", 
+         validate: nameInput => {
+            if (nameInput) {
+               return true;
+            } else {
+               console.log("Please enter your full name!");
+               return false;
+            }
+         }
+      },
+      {
+         type: "input",
+         name: "employeeId",
+         message: "Enter your employee ID",
+         validate: employeeId => {
+            if (employeeId) {
+               return true;
+            } else {
+               console.log("Please enter employee ID");
+               return false;
+            }
+         }
+      },
+      {
+         type: "input",
+         name: "email",
+         message: "Enter your email address",
+         validate: email => {
+            if (email) {
+               return true;
+            } else {
+               console.log("Please enter email address");
+               return false;
+            }
+         }
+      },
+      {
+         type: "input",
+         name: "school",
+         message: "Enter your school name",
+         validate: school => {
+            if (school) {
+               return true;
+            } else {
+               console.log("Please enter your current school name");
+               return false;
+            }
+         }
+      }
+   ]).then(answers => {
+      console.log(answers);
+      const intern = new Intern(answers.name, answers.employeeId, answers.email, answers.school);
+      roundTable.push(intern);
+      promptTeamMenu();
+   })
 };
 
+const assembleAvengers = () => {
+   if (!fs.existsSync(DIST_DIR)) {
+      fs.mkdirSync(DIST_DIR);
+   }
+   fs.writeFileSync(distpath, generateProfile(roundTable), "utf-8");
+};
 
-
-init();
+promptManager();
 
 
 
